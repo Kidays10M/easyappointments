a56be73c0ca6d9f0bdd1a5f9a2499d355401b484
Matías Bonvin <mat_bonvin@hotmail.com>
2025-07-07 22:48:20 +0200

    combination of the server-side rewrite rules (.htaccess) and the application-side configuration should resolve all the 404 errors and allow CodeIgniter's router to work correctly

--- application/config/config.php
+++ application/config/config.php
@@ -42,7 +42,7 @@ $config['base_url'] = !is_cli() ? trim($protocol . $domain . $request_uri, '/') 
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -60,7 +60,7 @@ $config['index_page'] = 'index.php';
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol'] = 'AUTO';
+$config['uri_protocol'] = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
